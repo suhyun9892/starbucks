e2862279dfd1c1f8423a85e47505c706b939680c
Suhyun Park <suhyun_4951@naver.com>
2024-06-07 11:53:27 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,21 +1,4 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-// seachEl 안에서 input 찾는다는 뜻
 
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  // 한번 검색요소를 잡았으면 js 명령을 통해서 focused 클래스를 추가하라는 뜻
-  searchInputEl.setAttribute('placeholder', '통합검색');
-  // html 속성 지정
-});
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
 
 const badgeEl = document.querySelector('header .badges');
 
@@ -143,5 +126,3 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller());
 });
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
